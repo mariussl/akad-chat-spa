4e5d864f94cdd42237bb9c367bd91b8ded99294f
marius <marius.lessiak@schubs.at>
2020-04-27 19:03:47 +0200

    fix dirs

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = "http://rose.schubs.at/akad-chat-api/api";
+export const apiUrl = "https://rose.schubs.at/akad-chat-api/api";
 export const apiUserUrl = apiUrl + "/user.php";
 export const apiRoomUrl = apiUrl + "/room.php";
 export const apiMessageUrl = apiUrl + "/message.php";
